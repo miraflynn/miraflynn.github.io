23b543725cb7de1281ccf5eedf10faa1ed5ff174
Mira Flynn <miraflynn0@gmail.com>
2022-02-01 12:00:37 -0500

    Updated github calendar script sources, some minor info

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,8 +41,7 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "miraflynn", { responsive: true });
-    
+    new GitHubCalendar(".github-graph", "miraflynn", { responsive: true, tooltips: true });
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "miraflynn", selector: "#ghfeed" });
